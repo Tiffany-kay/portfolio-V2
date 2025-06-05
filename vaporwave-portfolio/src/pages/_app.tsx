652e1c9cc5a92ee00d3f917bd4f2d056ee3f226c
Tiffany Gathoni <gathonitiffany16@gmail>
2025-06-05 12:00:20 +0300

    build changes

--- vaporwave-portfolio/src/pages/_app.tsx
+++ vaporwave-portfolio/src/pages/_app.tsx
@@ -1,8 +1,5 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import "@fontsource/orbitron/700.css";
-import "@fontsource/vt323/400.css";
-import "@fontsource/space-grotesk/400.css";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import Head from "next/head";
 
